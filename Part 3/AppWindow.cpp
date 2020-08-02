e05be7bae55dbbae07b522efd7388a1f98bc961a
NeilDG <fm.atropos@gmail.com>
2020-08-02 20:15:29 +0800

    First stable implementation of multiple object renderer.

---
+++ Part 3/AppWindow.cpp
@@ -0,0 +1,139 @@
+#include "AppWindow.h"
+#include "EngineTime.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+#include "ConstantBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexBuffer.h"
+#include "Vector3D.h"
+#include "Matrix4x4.h"
+#include "MathUtils.h"
+
+struct Vertex {
+	Vector3D position;
+	Vector3D color;
+	Vector3D color2;
+};
+
+_declspec(align(16)) //make CBData a size of 16-bytes.
+struct CBData {
+	Matrix4x4 worldMatrix;
+	Matrix4x4 viewMatrix;
+	Matrix4x4 projMatrix;
+	//unsigned int time; //size of only 4 bytes. this won't align in GPU device because device requires 16 bytes.
+	float time;
+};
+
+AppWindow* AppWindow::sharedInstance = NULL;
+
+AppWindow* AppWindow::getInstance()
+{
+	return sharedInstance;
+}
+
+void AppWindow::initialize()
+{
+	sharedInstance = new AppWindow();
+}
+
+void AppWindow::onCreate()
+{
+	Window::onCreate();
+	srand(time(NULL));
+	std::cout << "On create \n";
+}
+
+void AppWindow::onUpdate()
+{
+	this->ticks += EngineTime::getDeltaTime();
+	
+	/*if (this->ticks > this->CHANGE_DELAY) {
+		this->ticks = 0.0f;
+		this->displayAlt = !this->displayAlt;
+	}*/
+
+	GraphicsEngine* graphEngine = GraphicsEngine::getInstance();
+	DeviceContext* deviceContext = graphEngine->getImmediateContext();
+	deviceContext->setVertexShader(this->vertexShader);
+	deviceContext->setPixelShader(this->pixelShader);
+	deviceContext->clearRenderTargetColor(this->swapChain, 0, 0.5, 0.5, 1);
+
+	RECT windowRect = this->getClientWindowRect();
+	int width = windowRect.right - windowRect.left;
+	int height = windowRect.bottom - windowRect.top;
+
+	deviceContext->setViewportSize(width, height);
+
+	for (int i = 0; i < this->cubeList.size(); i++) {
+		this->cubeList[i]->update(EngineTime::getDeltaTime());
+		this->cubeList[i]->draw(width, height, this->vertexShader, this->pixelShader);
+	}
+	
+	graphEngine->getSwapChain()->present(true); //NOTE: Called once per screen refresh.
+}
+
+void AppWindow::onDestroy()
+{
+	Window::onDestroy();
+	this->vertexBuffer->release();
+	this->indexBuffer->release();
+	this->constantBuffer->release();
+	this->swapChain->release();
+	this->vertexShader->release();
+	this->pixelShader->release();
+	GraphicsEngine::destroy();
+	std::cout << "On destroy \n";
+}
+
+void AppWindow::createGraphicsWindow()
+{
+	GraphicsEngine::initialize();
+	GraphicsEngine* graphEngine = GraphicsEngine::getInstance();
+
+	this->swapChain = GraphicsEngine::getInstance()->getSwapChain();
+	RECT windowRect = this->getClientWindowRect();
+	int width = windowRect.right - windowRect.left;
+	int height = windowRect.bottom - windowRect.top;
+	std::cout << "Window rect width: " << width << "\n";
+	std::cout << "Window rect height: " << height << "\n";
+
+	this->swapChain->init(this->getWindowHandle(), width, height);
+
+	void* shaderByteCode = nullptr;
+	size_t sizeShader = 0;
+
+	//vertex stage
+	graphEngine->compileVertexShader(L"VertexShader.hlsl", "main", &shaderByteCode, &sizeShader);
+	this->vertexShader = graphEngine->createVertexShader(shaderByteCode, sizeShader);
+
+	for (int i = 0; i < 100; i++) {
+		float x = MathUtils::randomFloat(-0.75, 0.75f);
+		float y = MathUtils::randomFloat(-0.75, 0.75f);
+
+		Cube* cubeObject = new Cube("Cube", shaderByteCode, sizeShader);
+		cubeObject->setAnimSpeed(MathUtils::randomFloat(-3.75f, 3.75f));
+		cubeObject->setPosition(Vector3D(x, y, 0.0f));
+		cubeObject->setScale(Vector3D(0.25, 0.25, 0.25));
+		this->cubeList.push_back(cubeObject);
+	}
+
+	graphEngine->releaseCompiledShader(); //this must be called after each stage.
+
+	//pixel shading stage
+	graphEngine->compilePixelShader(L"PixelShader.hlsl", "main", &shaderByteCode, &sizeShader);
+	this->pixelShader = graphEngine->createPixelShader(shaderByteCode, sizeShader);
+	graphEngine->releaseCompiledShader();
+}
+
+
+
+AppWindow::AppWindow():Window()
+{
+	this->createWindow();
+}
+
+AppWindow::~AppWindow()
+{
+	Window::~Window();
+}
+
